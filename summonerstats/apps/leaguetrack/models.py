10544e6ce1ffc18c925d86683c15ee045d6d0c1e
Nicholas Thomson <nthomsondev@gmail.com>
2012-09-08 03:40:34 -0400

    Add better requirements for fields, add basic helper functions

--- summonerstats/apps/leaguetrack/models.py
+++ summonerstats/apps/leaguetrack/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.conf import settings
 
 class Summoner(models.Model):
     user = models.ForeignKey(User)
@@ -9,12 +10,28 @@ class Summoner(models.Model):
     losses = models.IntegerField()
     leaves = models.IntegerField()
 
+    def __unicode__(self):
+        return "%s (%s)" % (self.name, self.level)
+
 class Champion(models.Model):
     name = models.CharField(max_length=40)
     title = models.CharField(max_length=100)
     reference = models.IntegerField()
 
+    def __unicode__(self):
+        return "%s (%s)" % (self.full_name(), self.reference)
+
+    def portrait_url(self, size=120):
+        return "%simg/champions/%s/%s.png" % (settings.STATIC_URL, size, self.slug_name())
+
+    def slug_name(self):
+        return self.name.replace("'", "").replace(" ", "").replace(".", "").lower()
+
+    def full_name(self):
+        return "%s, %s" % (self.name, self.title)
+
 class Game(models.Model):
+    owner = models.ForeignKey(User)
     players = models.ManyToManyField(Summoner, through='Game_Player')
     match_type = models.IntegerField()
     game_mode = models.IntegerField()
@@ -24,6 +41,9 @@ class Game(models.Model):
     winning_team = models.IntegerField()
     match_length = models.IntegerField() #in seconds
 
+    def __unicode__(self):
+        return "%s: %ss (%s)" % (self.queue_type, self.match_length, self.region)
+
 class SummonerSpell(models.Model):
     name = models.CharField(max_length=40)
     description = models.TextField()
@@ -31,13 +51,19 @@ class SummonerSpell(models.Model):
     cooldown = models.IntegerField()
     bonus_cooldown = models.IntegerField()
 
+    def __unicode__(self):
+        return self.name
+
 class Item(models.Model):
     reference = models.IntegerField()
     name = models.CharField(max_length=100)
     price = models.IntegerField()
-    reduced_price = models.IntegerField()
+    reduced_price = models.IntegerField(blank=True, null=True)
     effect = models.TextField()
-    built_from = models.ManyToManyField("self", symmetrical=False)
+    built_from = models.ManyToManyField("self", symmetrical=False, blank=True)
+
+    def __unicode__(self):
+        return self.name
 
 class Game_Player(models.Model):
     game = models.ForeignKey(Game)
@@ -70,3 +96,6 @@ class Game_Player(models.Model):
     wins = models.IntegerField()
     losses = models.IntegerField()
     leaves = models.IntegerField()
+
+    def __unicode__(self):
+        return "%s (%s) - %s" % (self.player.name, self.champion.name, self.game.queue_type)
